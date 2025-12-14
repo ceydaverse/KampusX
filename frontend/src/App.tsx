92e1844bf7cd96bff3d9144e0244f7315eb268bf
layla <cerengenc589@gmail.com>
2025-12-15 01:20:38 +0300

    Temp commit before renaming Bildirim folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import AuthPage from "./features/auth/pages/AuthPage";
 import CategoryPage from "./features/categories/CategoryPage";
 import CommunityRulesPage from "./features/pages/CommunityRulesPage";
 import HakkimizdaPage from "./features/pages/HakkimizdaPage";
+import BildirimPage from './features/bildirim/pages/BildirimPage';
+
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/kategori/:slug" element={<CategoryPage />} />
         <Route path="/topluluk-kurallari" element={<CommunityRulesPage />} />
         <Route path="/hakkimizda" element={<HakkimizdaPage />} />
+        <Route path="/bildirimler" element={<Bildirimpage />} />
+
       </Routes>
     </BrowserRouter>
   );
